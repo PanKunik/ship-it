8ec3c3fff3d817e2e0e2c5b7f4cf8bda2aa4dd0f
PanKunik <patryk.kunicki@pm.me>
2026-06-29 14:34:13 +0200

    feat: discover applications from compose files

---
+++ src/ShipIt.Api/Models/ApplicationDefinition.cs
@@ -0,0 +1,8 @@
+﻿namespace ShipIt.Api.Models;
+
+public sealed class ApplicationDefinition
+{
+    public string Name { get; set; } = default!;
+    public string DirectoryPath { get; set; } = default!;
+    public string ComposeFile { get; set; } = default!;
+}
